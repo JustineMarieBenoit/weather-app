01e83c9611feb67b335d4bcd773406aadc4dd29c
Justine Benoit <justinemarie.benoit@gmail.com>
2020-05-22 09:27:56 -0400

    build

---
+++ build/precache-manifest.05ff783fba2698f3410bb6deab8ff23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678b748ecc0a55aad15c64f06a912446",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "63841cffd99f246cf418",
+    "url": "/weather-app/static/css/main.743f1a76.chunk.css"
+  },
+  {
+    "revision": "3e61278895e7faabc0e2",
+    "url": "/weather-app/static/js/2.7fc9d52a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.7fc9d52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63841cffd99f246cf418",
+    "url": "/weather-app/static/js/main.e53a649f.chunk.js"
+  },
+  {
+    "revision": "089fb1fee07ab9da1ce8",
+    "url": "/weather-app/static/js/runtime-main.029e69d5.js"
+  }
+]);
